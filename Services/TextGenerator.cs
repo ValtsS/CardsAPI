b954d7bc30d7eb30838dacf43e40d5686e5b6b2b
Valts Silaputnins <valts@velns.org>
2023-04-07 20:23:26 +0300

    Add more words

--- Services/TextGenerator.cs
+++ Services/TextGenerator.cs
@@ -8,9 +8,10 @@ public class TextGenerator
 {
     private readonly Random _random;
 
-    private readonly string[] _verbs = { "book", "reserve", "select", "choose", "get", "find" };
-    private readonly string[] _nouns = { "room", "suite", "bed", "pillow", "view", "balcony", "bathroom", "shower", "tub", "amenities", "hotel", "reservation", "guests", "booking" };
-    private readonly string[] _adjectives = { "luxurious", "cozy", "spacious", "comfortable", "modern", "elegant", "stylish", "sleek", "clean", "affordable", "beautiful" };
+    private readonly string[] _verbs = { "book", "reserve", "select", "choose", "get", "find", "explore", "browse", "view", "check", "compare", "search", "request", "confirm", "modify", "update", "pay", "review", "rate", "recommend" };
+    private readonly string[] _nouns = { "room", "suite", "bed", "pillow", "view", "balcony", "bathroom", "shower", "tub", "amenities", "hotel", "reservation", "guests", "booking", "check-in", "check-out", "lobby", "restaurant", "bar", "pool", "gym", "conference", "business", "wifi" };
+    private readonly string[] _adjectives = { "luxurious", "cozy", "spacious", "comfortable", "modern", "elegant", "stylish", "sleek", "clean", "affordable", "beautiful", "chic", "romantic", "relaxing", "secluded", "scenic", "quaint", "historic", "charming", "resort-like", "family-friendly", "pet-friendly", "accessible", "unique" };
+
 
     public TextGenerator(int seed)
     {
@@ -36,6 +37,7 @@ public class TextGenerator
         var adjectives = Enumerable.Range(1, _random.Next(1, 3)).Select(i => GetRandomWord(_adjectives));
         var nouns = Enumerable.Range(1, _random.Next(1, 5)).Select(i => GetRandomWord(_nouns));
 
-        return $"{string.Join(" ", adjectives)} {string.Join(" ", nouns)} - Book now at our top-rated hotel!";
+        string title = $"{string.Join(" ", adjectives)} {string.Join(" ", nouns)} - Book now at our top-rated hotel!";
+        return char.ToUpper(title[0]) + title.Substring(1);
     }
 }
